9247c085fb04ee328f28d320a8e3309f88243b88
gsime1 <gabriele.simeone@gmail.com>
2020-10-18 12:35:09 +0200

    first commit

---
+++ crawl_places_api.py
@@ -0,0 +1,86 @@
+import requests
+import json
+import time
+import csv
+from ristoranti_ire import r
+
+CORSI = ["45.480137,9.210898",
+         "45.456942,9.174997",
+         "45.455820,9.196308",
+         "45.479524,9.167362"]
+
+TYPES = ["restaurant"]
+
+RADIUS = '1000'  # km
+
+FIELDS = ['name',
+          'formatted_address',
+          'international_phone_number',
+          'website',
+          'rating']
+
+
+class GooglePlaces(object):
+
+    def __init__(self, apiKey):
+        super(GooglePlaces, self).__init__()
+        self.apiKey = apiKey
+
+    def search_places_by_coordinate(self, location, radius, types):
+        endpoint_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+        places = []
+        params = {
+            'location': location,
+            'radius': radius,
+            'types': types,
+            'key': self.apiKey
+        }
+        res = requests.get(endpoint_url, params=params)
+        results = json.loads(res.content)
+        places.extend(results['results'])
+        time.sleep(2)
+        while "next_page_token" in results:
+            params['pagetoken'] = results['next_page_token'],
+            res = requests.get(endpoint_url, params=params)
+            results = json.loads(res.content)
+            places.extend(results['results'])
+            time.sleep(2)
+        return places
+
+    def get_place_details(self, place_id, fields):
+        endpoint_url = "https://maps.googleapis.com/maps/api/place/details/json"
+        params = {
+            'placeid': place_id,
+            'fields': ",".join(fields),
+            'key': self.apiKey
+        }
+        res = requests.get(endpoint_url, params=params)
+        place_details = json.loads(res.content)
+        return place_details
+
+if __name__ == '__main__':
+
+    api = GooglePlaces(apiKey="AIzaSyCGm1lid569ygk75VZ-dtWtYxmQj474y3k")
+    restaurant_ls = []
+    doppioni = [x.casefold() for x in r]
+
+    for coordinates in CORSI:
+
+        places = api.search_places_by_coordinate(location=coordinates,
+                                                 radius=RADIUS,
+                                                 types=TYPES)
+        for place in places:
+            details = api.get_place_details(place['place_id'], fields=FIELDS)
+            name = details['result']['name'].casefold()
+            if name not in doppioni:
+                if not any(x in name for x in ['pizz', 'keba']):
+
+                    restaurant_ls.append({k : details['result'].get(k, None)
+                                          for k in FIELDS})
+                    doppioni.append(name)
+
+    with open('restaurants.csv', 'w', encoding='utf8', newline='') as output_file:
+        fc = csv.DictWriter(output_file,
+                            fieldnames=restaurant_ls[0].keys())
+        fc.writeheader()
+        fc.writerows(restaurant_ls)
---
+++ ristoranti_ire.py
@@ -0,0 +1,279 @@
+r = ["Baguttino",
+     "Baobab organic burger",
+     "Basara",
+     "Bianco Latte",
+     "Bio.it",
+     "Bioesserì porta nuova",
+     "BioToBe",
+     "Bjiork",
+     "Blue Majik",
+     "Blue Nami",
+     "Boa Boa",
+     "Botanical",
+     "Bowls and more",
+     "Bussarakham",
+     "california bakery",
+     "Casa Turati",
+     "Chateau Dufan",
+     "Cibò natura vegan",
+     "Classico Corso Como",
+     "Coedo",
+     "Crunchy Green",
+     "el porteno",
+     "Feel fusion city life",
+     "Fish Dancer",
+     "Fishbar de Milan",
+     "Flavors",
+     "Flower burger",
+     "Food United Nations",
+     "Genuino",
+     "Gialle & Co",
+     "God save the food Piave",
+     "Good Blue",
+     "green day parini",
+     "Haru sushi fusion",
+     "Hats Off",
+     "igiban",
+     "Insaluteria",
+     "italian creative salad",
+     "Itinerario",
+     "Japonito",
+     "Jigas",
+     "Konami Evo",
+     "La forchetta verde",
+     "La Mantia",
+     "Light kitchen",
+     "Lo zappatore",
+     "Locanda pandenus",
+     "Lon Fon",
+     "Macha cafè",
+     "Maido",
+     "Maui Poke",
+     "Migjorn Cafè",
+     "Mint cafe milan",
+     "Miscusi",
+     "momo",
+     "Mu corso como",
+     "Mu Dim Sum",
+     "Muzzi",
+     "Namaste",
+     "Old wild west sempione",
+     "Osaka",
+     "Ottolina cafè",
+     "Pampa",
+     "Pandenus aulenti",
+     "Parco Sushi",
+     "Piedra del sol",
+     "Primé",
+     "rainbow milano",
+     "Sagami",
+     "Shiro Poporoya",
+     "Soulgreen",
+     "Special hamburgeria",
+     "Stendhal",
+     "Sushi Kobbo",
+     "Tagliabrodo",
+     "Takumi ramen",
+     "Tartarè",
+     "Tartare and Friends",
+     "Testami",
+     "Tha Dhaba",
+     "that's vapore",
+     "The Grill",
+     "the meatball family",
+     "the pure",
+     "this is not a sushi bar",
+     "tiki poke",
+     "Tripburger",
+     "verdi's kitchen",
+     "Yang sushi",
+     "Frankie's",
+     "Nu Cube",
+     "Ratanà",
+     "El Pastee",
+     "Full self service",
+     "SUSHIMI",
+     "Ristorante Berton",
+     "Heguì - Taste of Asia",
+     "Zushi Milano",
+     "Hasu",
+     "Il Sapore Giusto",
+     "Hayashi",
+     "Mister Panzerotto 4",
+     "Ristorante Lucca",
+     "Nara Sushi Restaurant",
+     "Taiyo Sushi Restaurant",
+     "La contea",
+     "da Mimmo 3",
+     "Sushi Plus",
+     "Tramé - Pisani",
+     "Little Italy Milano",
+     "Cuvée",
+     "Shibuya Milano - Ristorante Giapponese - All you can eat",
+     "Ristorante MyPuglia",
+     "Il Panzerotto - Made In Puglia",
+     "中餐厅Ristorante Cinese Antica Cina（米兰中餐)",
+     "唐阁 T'ang Court",
+     "Let's Wok Milano",
+     "Panetteria e pasticceria Romano",
+     "Mr. Miao",
+     "PRONTO PER ME",
+     "Gyoza House",
+     "2Mila Venti",
+     "Donna Titina",
+     "Koki",
+     "Cose Buone",
+     "L' Oasi",
+     "Il Faraone",
+     "Rambo",
+     "Oren",
+     "Cinder Mount",
+     "Panificio Maria Marinoni",
+     "Apulia Bakery",
+     "Genius",
+     "Corriere Espresso - Bike Corriere",
+     "Sciuscià Milano",
+     "Vivo e mangio bene",
+     "gelateria creativa Wally GRAMSCI",
+     "Italmenu",
+     "This is Not a Sushi Bar - Sanzio",
+     "Pasta mi",
+     "Larky delivery",
+     "Garden City",
+     "Peck",
+     "Ristorante Perla d'Oro",
+     "This Is Not a Sushi Bar - Porta Ticinese",
+     "cuoca@casa - chef",
+     "Hollywood",
+     "Il Cucchiaio di Legno",
+     "Castello Nuovo",
+     "Gastronomia Yamamoto",
+     "Sapurì",
+     "Basara - sushi pasticceria",
+     "Streeteat",
+     "142 Restaurant",
+     "Wafu",
+     "Feat Food",
+     "Eccellenze Campane Milano",
+     "La Magolfa & Go - Darsena",
+     "Ai Balestrari sul Naviglio Pavese",
+     "Mohemed Abdel Salam Elgendgg Ahmed",
+     "rosso rubino",
+     "Pescheria I Pesciolini - Milano",
+     "Taverna Greca Stelios",
+     "NERASUSHI",
+     "Bar Gabbiano",
+     "San Francesco 2",
+     "Rosebymary",
+     "Xier",
+     "Licolì Milano",
+     "Deep Milano",
+     "Fonte del Dolce",
+     "Trattoria Fonte",
+     "Trattoria Giada",
+     "Specialità egiziane Casa Nostra",
+     "Superstar 13",
+     "Mundial Break",
+     ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

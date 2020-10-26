ff02d5fb91773219acd6d1e76e4bd02820d0c2db
gabriele <gabriele>
2020-10-26 12:21:29 +0100

    added line to register user in keyring

--- batch_gmail.py
+++ batch_gmail.py
@@ -5,15 +5,15 @@ from mail_header import sender_id, sender_password
 from mail_header import subject, in_copy
 from email_body import body_txt
 
-yag = yagmail.SMTP(sender_id)
 
 if __name__ == "__main__":
 
-	yagmail.register(sender_id, sender_password)
+    yagmail.register(sender_id, sender_password)
+    yag = yagmail.SMTP(sender_id)
 
-	for receiver in receivers:
-		yag.send(
-		to=receiver,
-		cc=in_copy,
-		subject=subject,
-		contents=body_txt)
+    for receiver in receivers:
+
+        yag.send(to=receiver,
+                 cc=in_copy,
+                 subject=subject,
+                 contents=body_txt)
